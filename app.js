e38dcf0d38c9c43c998aa784b857f597d53a8b05
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2016-01-14 19:00:19 -0500

    app comments

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
+// code that uses ES6 features
 function add (a, b = 1) {
   console.log('adding', a, b)
   return a + b
 }
+
+// "user interface" code - not important
 function sum () {
   const a = parseInt(document.getElementById('a').value, 10)
   const b = parseInt(document.getElementById('b').value, 10) || undefined
